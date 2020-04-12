67a18f473deae332c57eda830bae5951e53c5338
sophie <s.hautcoeur@gmail.com>
2020-04-12 10:06:25 +0300

    changed port env variable

--- server.js
+++ server.js
@@ -121,6 +121,6 @@ app.put('/image', (req, res) => {
     .catch(err => res.status(400).json('Unable to get count'))
 })
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('app is running on port ${process.env.PORT}');
 })
