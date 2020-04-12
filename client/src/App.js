60618f044d7b14599ad27fa0b1d11cc29b5e1c0f
sophie <s.hautcoeur@gmail.com>
2020-04-12 10:11:48 +0300

    updated url

--- client/src/App.js
+++ client/src/App.js
@@ -58,7 +58,7 @@ class App extends React.Component {
 
   onSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://fooddetector.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -68,7 +68,7 @@ class App extends React.Component {
         .then(response => response.json())
         .then(response => {
           if (response) {
-            fetch('http://localhost:3000/image', {
+            fetch('https://fooddetector.herokuapp.com/image', {
               method: 'put',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
